8b35e8cf51450c9fabbcea1b14e55866b2226f89
dhens <dawsonhensel@gmail.com>
2020-08-26 04:33:44 -0700

    we now have a functional blog form that properly uses useState to handle form values

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import PostTitle from './components/PostTitle';
 import './App.css';
-import PostBody from './components/PostBody';
-import SubmitButton from './components/SubmitButton';
 import ComposePostWrapper from './components/ComposeForm';
+import ComposeForm from './components/ComposeForm';
 
 function App() {
   return (
     <div className="App">
       <ComposePostWrapper>
-        <PostTitle/>
-        <PostBody/>
-        <SubmitButton/>
+        <ComposeForm/>
       </ComposePostWrapper>
     </div>
   );
--- src/components/ComposeForm/index.js
+++ src/components/ComposeForm/index.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ComposeForm = props => {
+
+    const [state, setState] = useState({
+        title: '',
+        body: ''
+    });
+
+    const handleChange = event => {
+        const formValue = event.target.value;
+        setState({
+            ...state,
+            [event.target.name]: formValue
+        });
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        alert(`Submitting ${state.title} and ${state.body}`);
+    }
+
     return (
-        <form {...props} />
+        <form>
+            <label>
+                Title
+                <input onChange={handleChange} value={state.title} type="text" name="title"/>
+            </label>
+            <label>
+                Body
+                <textarea onChange={handleChange} value={state.body} name="body"/>
+            </label>
+            <input onClick={handleSubmit} type="submit" name="submit" value="Submit"/>
+        </form>
     )
 }
 
